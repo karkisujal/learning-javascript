94e0a02069aa4b4e87b490b9370e36b48f13f902
Sujal Karki <karkisujal2008@gmail.com>
2022-07-21 13:13:25 +0545

    called one function inside another javascript function

--- day2/script.js
+++ day2/script.js
@@ -141,3 +141,38 @@ function calculateArea(r){
 var radius = 5.2,
     theArea = calculateArea(radius);
 console.log("The ares is: " + theArea);
+
+
+
+function clunk(times){
+    var num = times;
+    while(num > 0){
+        display("clunk");
+        num = num - 1;
+    }
+}
+
+function thingamajig(size){
+    var facky = 1;
+    clunkCounter = 0;
+    if(size == 0){
+        display("clank");
+    } else if(size == 1){
+        display("thunk");
+    } else {
+        while(size > 1){
+            facky = facky * size;
+            size = size - 1;
+        }
+        clunk(facky);
+    }
+}
+
+function display(output){
+    console.log(output);
+    clunkCounter = clunkCounter + 1;
+}
+var clunkCounter = 0;
+thingamajig(5);
+console.log(clunkCounter);
+
