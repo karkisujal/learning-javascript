e7213fd96577a27b305ae6c93c1cdf27e49b4068
Sujal Karki <karkisujal2008@gmail.com>
2022-07-21 11:50:49 +0545

    Added function to variable

--- day2/script.js
+++ day2/script.js
@@ -82,4 +82,47 @@ function bark(dogName, dogWeight, comparision){
     }
 }
 
-bark("spike", '13', '20');
+bark("spike", "20", '20');
+
+
+function whatShallIWear(temp){
+    if(temp < 60) {
+        console.log("Wear a jacket");
+    } else if(temp < 70){
+        console.log("Wear a sweater");
+    } else {
+        console.log("Wear t-shirt");
+    }
+}
+
+whatShallIWear(50);
+whatShallIWear(80);
+whatShallIWear(60);
+
+
+function doIt(param) {
+    param = 2;
+}
+var test = 1;
+doIt(test);
+console.log(test);
+
+
+
+function bake(degrees){
+    var message;
+    if(degrees > 500) {
+        message = "I'm not a nuclear reactor!";
+    } else if(degrees < 100){
+        message = "I'm not a refrigerator!";
+    } else {
+        message = "That's a very comfortable temperature for me.";
+       // setMode("bake");
+       // setTemp("degrees");
+    }
+    return message;
+}
+
+var status = bake(350);
+
+console.log(status);
