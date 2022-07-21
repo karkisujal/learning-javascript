f26f7bd0fac324e94dca3a0447da3fc156a7b8d4
Sujal Karki <karkisujal2008@gmail.com>
2022-07-21 16:56:51 +0545

    Coparing two different array to get the best result

--- day2/script.js
+++ day2/script.js
@@ -208,7 +208,8 @@ function makePhrases(){
 makePhrases();
 
 
-var scores = [60, 50, 60, 58, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
+var scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
+var costs = [.25, .27, .25, .25, .25, .25, .33, .31, .25, .29, .27, .22, .31, .25, .25, .33, .21, .25, .25, .25, .28, .25, .24, .22, .20, .25, .30, .25, .24, .25, .25, .25, .27, .25, .26, .29]
 var output,
     i = 0;
 /*while(i < (scores.length - 1) ){
@@ -264,6 +265,25 @@ function getBestResults(scores, highScore){
 }
 var bestSolutions = getBestResults(scores, highScore);
 console.log("Solutions with the highest score: " + bestSolutions);
+
+function getMostCostEffectiveSolution(scores, costs, highScore){
+    var cost = 100,
+        index;
+    for (var i = 0; i < scores.length; i++){
+        if(scores[i] == highScore){
+            if(cost > costs[i]){
+                index = i;
+                cost = costs;
+            }
+        }
+    }
+    return index;
+}
+var mostCostEffective = getMostCostEffectiveSolution(scores, costs, highScore);
+console.log("Bubble Solution #" + mostCostEffective + " is the most cost effective");
+
+
+
 
 var product = ["Choo Choo Chocolate", "Icy Mint", "Cake Batter", "Bubblegum"];
 var hasBubbleGum = [false, false, false, true];
