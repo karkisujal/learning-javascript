231e8b5ec9b76305f73926db558aa166c142dbb1
Sujal Karki <karkisujal2008@gmail.com>
2022-07-21 14:41:58 +0545

    Started with javascript array

--- day2/script.js
+++ day2/script.js
@@ -176,3 +176,33 @@ var clunkCounter = 0;
 thingamajig(5);
 console.log(clunkCounter);
 
+
+var balance = 10500,
+    cameraOn = true;
+
+function steal(balance, amount){
+    cameraOn = false;
+    if(amount < balance){
+        balance = balance - amount;
+    }
+    return amount;
+    cameraOn = true;
+}
+
+var amount = steal(balance, 1250);
+alert("Criminal: you stole " + amount + "!");
+
+
+function makePhrases(){
+    var words1 = ["24/7", "multi-tier", "30,3000 foot", "B-to-B", "win-win"];
+    var words2 = ["empowered", "value-added", "oriented", "focused", "aligned"];
+    var words3 = ["process", "solution", "tipping-pont", "strategy", "vision"];
+
+    var rand1 = Math.floor(Math.random() * words1.length);
+    var rand2 = Math.floor(Math.random() * words2.length);
+    var rand3 = Math.floor(Math.random() * words3.length);
+
+    var phrase = words1[rand1] + " " + words2[rand2] + " " + words3[rand3];
+    alert(phrase);
+}
+makePhrases();
